913a3150531c250678aea3584858daf272aee61c
dariobetancourth <156965416+dariobetancourth@users.noreply.github.com>
2024-02-10 01:42:27 -0700

    Data

---
+++ database/data/UserAddress_data.sql
@@ -0,0 +1,32 @@
+INSERT INTO UserAddress (user_id,address_1,address_2,city,st,zip,country)
+    VALUES (
+        1,
+        '578 Lake St',
+        '',
+        'Lake City',
+        'UT',
+        '84101',
+        'United States'
+        );
+
+INSERT INTO UserAddress (user_id,address_1,address_2,city,st,zip,country)
+    VALUES (
+        2,
+        '578 Lake St',
+        '',
+        'Lake City',
+        'UT',
+        '84101',
+        'United States'
+        );
+
+INSERT INTO UserAddress (user_id,address_1,address_2,city,st,zip,country)
+    VALUES (
+        3,
+        '578 Lake St',
+        '',
+        'Lake City',
+        'UT',
+        '84101',
+        'United States'
+        );
---
+++ database/data/User_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO User (first_name,last_name,email, pass_word, created_date,is_active)
+    VALUES (
+        'Alex',
+        'Smith',
+        'alex.smith@ensign.edu',
+        'password',
+        '2024-01-18 10:15:03',
+        TRUE
+        );
+INSERT INTO User (first_name,last_name,email, pass_word, created_date,is_active)
+    VALUES (
+        'Sariah',
+        'Nelson',
+        'sariah.nelson@ensign.edu',
+        'password',
+        now(),
+        TRUE
+        );
