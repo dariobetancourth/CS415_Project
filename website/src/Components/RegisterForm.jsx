dd61f434bb4bc08163666a45865bb2bedd0c32c5
dariobetancourth <156965416+dariobetancourth@users.noreply.github.com>
2024-03-08 20:48:35 -0700

    1

--- website/src/Components/RegisterForm.jsx
+++ website/src/Components/RegisterForm.jsx
@@ -21,7 +21,7 @@ export const RegisterForm = (props) => {
             pass_word: pass
         })
         try {
-            fetch(process.env.REACT_APP_API_URL_BASE + '/users/',{
+            fetch('http://localhost:3001/users/',{
             method: 'POST',
             body: payload,
             headers:{
